03bef0a9b8276ed2af3feeb5af9c7730f080f988
Kelly Simmons <kelly.l.simmons1@gmail.com>
2021-04-12 10:27:16 -0400

    added admin

--- vue/src/views/AdminHome.vue
+++ vue/src/views/AdminHome.vue
@@ -1,16 +1,5 @@
 <template>
-
-
-<div>
-  <doctor-home v-if="$store.state.currentUserRole == 'ROLE_DOCTOR'"></doctor-home>
-
-  <admin-home v-if="$store.state.currentUserRole == 'ROLE_ADMIN'"></admin-home>
-
-  <v-container grid-list-md fluid color="primary" v-if="($store.state.currentUserRole == 'ROLE_USER')">
-
-
-
-  
+<v-container grid-list-md fluid >
 
   <v-app-bar class="px-5" app color="primary" flat>
     <v-tabs 
@@ -20,8 +9,6 @@
     <router-link :to="{ name: 'home' }" style="text-decoration: none" >
       <v-tab class="pa-6 mx-auto" style="color:white">Home</v-tab>
     </router-link>
-
-    
     <v-divider vertical></v-divider>
       
     <router-link v-bind:to="{ name: 'logout' }" 
@@ -32,45 +19,46 @@
     <v-divider vertical></v-divider>
 
     <v-tab class="pa-6"  style="color:white">
-      Welcome, {{$store.state.currentPatient.firstName}} {{$store.state.currentPatient.lastName}}!
+      Welcome, Admin!
      <v-avatar
         class="hidden-sm-and-down mx-2"
         color="grey darken-1 shrink"
         size="38"
       >
-      <v-icon dark>
+      <v-icon dark >
         mdi-account-circle
       </v-icon>
       </v-avatar>
       </v-tab >
     </v-tabs >
-
     </v-app-bar>
+    <!-- appointtent availablity -->
+   
+      
+     
+        
 
+      <v-container >
+        <v-row>
+          <v-col cols="12" md="12">
+              <v-card id="docs"
+                  rounded="lg"
+                  min-height="100">
+                  <p>We will put drag and drop office stuff here next sprint. </p>
+              </v-card>
+          </v-col>
+        </v-row>
+      </v-container>
 
-
-       
-      <doctors-list />
-
+     
+    
 </v-container>
-</div>
 </template>
 
 <script>
-import DoctorsList from '@/components/DoctorsList'
-import DoctorHome from '@/views/DoctorHome'
-import AdminHome from '@/views/AdminHome'
-
 
 export default {
-  name: "home",
-  components: {
-    DoctorsList,
-    DoctorHome, 
-    AdminHome
-  }
+  name: "admin_home",
+ 
 };
-
-
 </script>
-
