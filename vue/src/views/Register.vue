5001286f8c6470280c73b64318b5939e3cedb487
olyame <ola.melnichenko10@gmail.com>
2021-04-15 16:39:06 -0400

    Editing registration

--- vue/src/views/Register.vue
+++ vue/src/views/Register.vue
@@ -115,11 +115,11 @@
           </router-link>
         </v-card-actions>
         <v-divider></v-divider>
-        <v-card-actions >
+        <v-card-actions class="ml-3 mr-7">
           <v-btn :disabled= !valid 
             block
             type="submit" 
-            form="form-register" class="mr-4 white--text"
+            form="form-register" class="white--text"
             @click="validate"  id="sign-in-button">Register</v-btn>
         </v-card-actions>
       </v-card>
